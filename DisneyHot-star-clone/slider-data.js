c36d3c3875e4bed5a9eaf9588dd6b89c2ea2215f
Ashfaqueshaik <135572894+Ashfaqueshaik@users.noreply.github.com>
2023-06-26 16:24:05 +0530

    Add files via upload

---
+++ DisneyHot-star-clone/slider-data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'WI  vs IND 305/6 (50)',
+        desc: 'No Super Over, but theres no shortage of entertainment here in Trinidad as we draw curtains on this thrill-a-minute encounter where West Indies finished second despite scoring 50/0 off the last 5 overs',
+        image: 'img/slider2.webp'
+    },
+    {
+        name: 'Rudra: The Edge of Darkness',
+        desc: 'Rudra: The Edge of Darkness is an Indian psychological crime thriller streaming television series, created for Disney+ Hotstar. It is a remake of British series Luther. The series stars Ajay Devgn, Raashii Khanna and Esha Deol.',
+        image: 'img/slider1.webp'
+    },
+    {
+        name: 'The Book of Boba Fett',
+        desc: 'The Book of Boba Fett is an American space Western television miniseries created by Jon Favreau for the streaming service Disney+. It is part of the Star Wars franchise and a spin-off from the series The Mandalorian.',
+        image: 'img/slider3.webp'
+    },
+    {
+        name: 'raya and the last dragon',
+        desc: 'Raya and the Last Dragon (/ˈraɪ.ə/ RYE-ə) is a 2021 American computer-animated fantasy action-adventure film produced by Walt Disney Animation Studios and distributed by Walt Disney Studios Motion Pictures.',
+        image: 'img/slider4.PNG'
+    },
+    {
+        name: 'luca',
+        desc: 'Luca is a 2021 American computer-animated coming-of-age fantasy film produced by Pixar Animation Studios for Walt Disney Pictures.',
+        image: 'img/slider5.PNG'
+    }
+]
